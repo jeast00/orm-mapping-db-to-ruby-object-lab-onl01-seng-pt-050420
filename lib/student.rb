0cf82f462b93a8cd345f5ff91ee44e256ad4650c
Jonathan East <jonathan_east@ymail.com>
2020-07-06 14:23:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,11 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(num)
-
+    sql = <<-SQL
+        SELECT COUNT(grade)
+        FROM students
+        WHERE num = ? AND grade = '10'
+      SQL
   end
 
   def self.first_student_in_grade_10
