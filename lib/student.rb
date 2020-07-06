625b728345810be367b19bd859cdea31b82f8f40
Jonathan East <jonathan_east@ymail.com>
2020-07-06 14:37:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,12 +82,12 @@ class Student
 
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-        SELECT *
+        SELECT num
         FROM students
         WHERE grade = '10'
       SQL
 
-      DB[:conn].execute(sql, num).map { |row| self.new_from_db(row) }
+      DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   end
 
   def self.first_student_in_grade_10
